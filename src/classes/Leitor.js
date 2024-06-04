b90aff2c06497de9b703e5d1c2c8004df5cb5a69
Deyse Aiala <deyseaiala23@gmail.com>
2024-06-03 22:07:48 -0300

    Adicionando lógica para criar, deletar e consultar leitor

--- src/classes/Leitor.js
+++ src/classes/Leitor.js
@@ -1,9 +1,49 @@
+const crypto = require("crypto")
+
 class Leitor {
 
-    constructor() {
-       
+    #codigo = "";
+    #nome = "";
+    #cpf = "";
+    #dataNascimento = "";
+
+    constructor(nome) {
+       this.#nome = nome.toLowerCase()
+       this.#codigo = crypto.randomUUID()
+    }
+
+    get getNome(){
+        return this.#nome
+
+    }
+
+    /*set setNome(nome){
+        this.#nome = nome
+        }*/
+
+    get getCpf(){
+        return this.#cpf
+
+    }
+
+    set setCpf(cpf){
+        this.#cpf = cpf
+        }
+    
+    get getDataNascimento(){
+        return this.#dataNascimento
+
+    }
+
+    set setDataNascimento(dataNascimento){
+        this.#dataNascimento = dataNascimento
+        }
+
+    get getCodigo(){
+        return this.#codigo 
     }
 
 }
 
 module.exports = Leitor;
+
--- src/classes/LeitorCrud.js
+++ src/classes/LeitorCrud.js
@@ -1,6 +1,61 @@
-class LeitorCrud {
+const fs = require("fs");
+
+class LeitorCrud { 
 
     constructor() {
+        this.filePath = "./src/files/leitores.json";
+    }
+
+    criar(novoLeitor){
+
+        const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
+
+        conteudoAtual.push({
+            codigo: novoLeitor.getCodigo,
+            nome: novoLeitor.getNome,
+            cpf:  novoLeitor.getCpf,
+            dataNascimento: novoLeitor.getDataNascimento
+         })
+
+        fs.writeFileSync(
+            this.filePath,
+            JSON.stringify(conteudoAtual, null, 2),
+            'utf-8'
+        )
+    }
+
+    consultar(palavraPesquisada){
+
+        const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
+
+        const leitorEncontrado = conteudoAtual.find(leitor => leitor.nome === palavraPesquisada || leitor.codigo === palavraPesquisada)
+
+        if(leitorEncontrado) {
+            console.log(leitorEncontrado)
+        } else {
+            console.log("Leitor não encontrado.")
+        }
+
+    }
+
+    async deletar(codigo){
+
+        const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
+
+        const leitorEncontrado = conteudoAtual.find((leitor) => leitor.codigo === codigo)
+        if(leitorEncontrado){
+            const leitoresAtuais = conteudoAtual.filter((leitor) => leitor.codigo !== codigo)
+            console.log("Leitor excluído com sucesso!")
+             fs.writeFileSync(
+            this.filePath,
+            JSON.stringify(leitoresAtuais, null, 2),
+            'utf-8'
+        ) 
+        } else{
+            console.log("Leitor não encontrado.")
+        }
+
+        
        
     }
 
